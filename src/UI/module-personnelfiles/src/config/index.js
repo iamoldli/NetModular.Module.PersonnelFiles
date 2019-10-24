9d00f972085fc6c734c20dc55d84e73738aa58c4
oldli <wenju1991@163.com>
2019-10-24 13:54:14 +0800

    update

--- src/UI/module-personnelfiles/src/config/index.js
+++ src/UI/module-personnelfiles/src/config/index.js
@@ -1,11 +1,11 @@
 const isDev = process.env.NODE_ENV !== 'production'
 
 const config = {
-  baseUrl: ''
+  baseUrl: '/api/'
 }
 
 // 开发模式
 if (isDev) {
-  config.baseUrl = 'http://localhost:6227/api/'
+  config.baseUrl = 'http://localhost:6224/api/'
 }
 export default config
